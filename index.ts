af57f86a4c388bf55bd31cfd1518deca0550b3d2
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-07-30 01:24:53 -0400

    reorganize

--- index.ts
+++
@@ -1,2 +0,0 @@
-export { LongPressDirective } from './src/directives/long-press.directive';
-export { LongPressModule } from './src/long-press.module';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { LongPressDirective } from './directives/long-press.directive';
+export { LongPressModule } from './long-press.module';
