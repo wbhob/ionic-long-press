4924f9b5afb9959707a15f36642b8347085b7180
Robin Giel <robingiel@gmail.com>
2019-09-06 11:08:53 +0200

    remove gesture

--- src/long-press.module.ts
+++ src/long-press.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Gesture } from 'ionic-angular/gestures/gesture';
 import { LongPressDirective } from './directives/long-press.directive';
 
 @NgModule({
